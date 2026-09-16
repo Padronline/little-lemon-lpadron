c480d9023bffe7b83eca010b6075f780278ba055
Padronline <padronline@gmail.com>
2026-09-16 19:12:06 -0300

    Expand footer with contact and social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,34 @@
 function Footer() {
   return (
-    <footer className="footer">
-      <h2 className="logo">Little Lemon</h2>
-      <ul className="footer-list">
-        <li><a href="/">Home</a></li>
-        <li><a href="/">About</a></li>
-        <li><a href="/">Menu</a></li>
-        <li><a href="/booking">Reservations</a></li>
-        <li><a href="/">Order Online</a></li>
-        <li><a href="/">Login</a></li>
-      </ul>
-      <p>123 Chicago Ave, Chicago</p>
-      <p>Contact: (312) 555-0100</p>
+    <footer className="footer" id="contact">
+      <div className="footer-columns">
+        <div className="footer-column">
+          <h3 className="logo">Little Lemon</h3>
+          <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#testimonials">Testimonials</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h4>Contact</h4>
+          <p>123 Chicago Ave, Chicago, IL</p>
+          <p>(312) 555-0100</p>
+          <p>hello@littlelemon.com</p>
+        </div>
+
+        <div className="footer-column">
+          <h4>Social Media</h4>
+          <ul>
+            <li><a href="/">Facebook</a></li>
+            <li><a href="/">Instagram</a></li>
+            <li><a href="/">Twitter</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <p className="footer-copy">Copyright Little Lemon</p>
     </footer>
   );
 }
