cf3611daf511dffa9f63be1cc4ddaae7fa3dfd85
Padronline <padronline@gmail.com>
2026-09-16 19:19:44 -0300

    Add logo image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import Nav from './Nav';
 function Header() {
   return (
     <header className="header">
-      <h1 className="logo">Little Lemon</h1>
+      <div className="logo-group">
+        <img src="https://placehold.co/40x40/f4ce14/495e57?text=LL" alt="Little Lemon logo" className="logo-img" />
+        <h1 className="logo">Little Lemon</h1>
+      </div>
       <Nav />
     </header>
   );
