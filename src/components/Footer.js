0e589e8cb7a697c4cbd3e46d5029c396b82bee03
Padronline <padronline@gmail.com>
2026-09-16 18:49:49 -0300

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <h2 className="logo">Little Lemon</h2>
+      <ul className="footer-list">
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Menu</a></li>
+        <li><a href="/booking">Reservations</a></li>
+        <li><a href="/">Order Online</a></li>
+        <li><a href="/">Login</a></li>
+      </ul>
+      <p>123 Chicago Ave, Chicago</p>
+      <p>Contact: (312) 555-0100</p>
+    </footer>
+  );
+}
+
+export default Footer;
